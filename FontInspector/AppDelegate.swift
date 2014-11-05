9fa03d78deb07826acca32698b0fa8c54e539ca7
Litherum <litherum@gmail.com>
2014-11-04 23:51:01 -0800

    Outline view bindings

--- FontInspector/AppDelegate.swift
+++ FontInspector/AppDelegate.swift
@@ -8,14 +8,59 @@
 
 import Cocoa
 
+class CodepointsValueTransformer : NSValueTransformer {
+    override class func transformedValueClass() -> AnyClass {
+        return NSString.self
+    }
+    
+    override class func allowsReverseTransformation() -> Bool {
+        return false
+    }
+    
+    override func transformedValue(value: AnyObject?) -> AnyObject? {
+        if let specificValue = value as? NSNumber {
+            return String(UnicodeScalar(specificValue.integerValue))
+        }
+        return nil
+    }
+    override func reverseTransformedValue(value: AnyObject?) -> AnyObject? {
+        return nil
+    }
+}
+
+class CodepointNode: NSObject {
+    var isLeaf: Bool
+    var children: [CodepointNode]
+    var codepoint: UnicodeScalar
+
+    init(isLeaf: Bool, children: [CodepointNode], codepoint: UnicodeScalar) {
+        self.isLeaf = isLeaf
+        self.children = children
+        self.codepoint = codepoint
+    }
+}
+
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
+    @IBOutlet weak var codepointController: NSTreeController!
 
+    var codepoints: NSArray
+
+    override init() {
+        codepoints = []
+    }
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        NSValueTransformer.setValueTransformer(CodepointsValueTransformer(), forName: "CodepointsValueTransformer")
+
+        let object1 = CodepointNode(isLeaf: false, children: [], codepoint: UnicodeScalar(68))
+        let object2 = CodepointNode(isLeaf: true, children: [], codepoint: UnicodeScalar(111))
+        let keypath1 = NSIndexPath(index: 0);
+        let keypath2 = keypath1.indexPathByAddingIndex(0);
+        codepointController.insertObject(object1, atArrangedObjectIndexPath: keypath1)
+        codepointController.insertObject(object2, atArrangedObjectIndexPath: keypath2)
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
